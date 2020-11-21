ebecb1ba4eba0983965e05d4852169b9e8db4c9d
lautarodapin <lautarodapin@gmail.com>
2020-11-21 11:12:22 -0300

    env

--- portal_news/portal_news/settings.py
+++ portal_news/portal_news/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g9pmp7xmzd*6tu^!8p)lu)_gy%%25idk)qsc(lae(biv&y5)#u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = [".heroku.com", "127.0.0.1", "localhost",]
 
