4a4959eb766a990716d28ca5d9595bb9d8539abb
lautarodapin <lautarodapin@gmail.com>
2020-11-21 18:40:23 -0300

    cambio en databases

--- portal_news/portal_news/settings.py
+++ portal_news/portal_news/settings.py
@@ -88,17 +88,15 @@ WSGI_APPLICATION = 'portal_news.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 if AMBIENTE == 'PROD':
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
     
-elif AMBIENTE == 'DEV':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 # Password validation
