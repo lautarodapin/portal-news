6e2f72365edee000a262bee4e9925ad1190eea70
lautarodapin <lautarodapin@gmail.com>
2020-11-21 16:49:51 -0300

    probando django-heroku

--- portal_news/portal_news/settings.py
+++ portal_news/portal_news/settings.py
@@ -145,3 +145,9 @@ TINYMCE_DEFAULT_CONFIG = {
 }
 # Whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
