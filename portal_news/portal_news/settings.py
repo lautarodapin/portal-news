0504509944d2fb76996c1e28d20707a364d96487
lautarodapin <lautarodapin@gmail.com>
2020-11-21 18:09:10 -0300

    añadida database postgress

--- portal_news/portal_news/settings.py
+++ portal_news/portal_news/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+AMBIENTE = config('AMBIENTE', default='PROD')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -88,12 +89,16 @@ WSGI_APPLICATION = 'portal_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if AMBIENTE == 'PROD':
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    
+elif AMBIENTE == 'DEV':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
