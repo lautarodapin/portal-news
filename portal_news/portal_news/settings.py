2af0a8e5936f240db794076eac6c6a69dcad661c
lautarodapin <lautarodapin@gmail.com>
2020-11-29 20:17:06 -0300

    Añadido cast bool a debug

--- portal_news/portal_news/settings.py
+++ portal_news/portal_news/settings.py
@@ -25,7 +25,7 @@ AMBIENTE = config('AMBIENTE', default='PROD')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ["*",]
 
