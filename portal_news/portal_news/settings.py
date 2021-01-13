e1ae81bec0a34e3386609a7f70e3eab955aaeee8
lautarodapin <lautarodapin@gmail.com>
2021-01-13 18:10:54 -0300

    settings change

--- portal_news/portal_news/settings.py
+++ portal_news/portal_news/settings.py
@@ -230,7 +230,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
